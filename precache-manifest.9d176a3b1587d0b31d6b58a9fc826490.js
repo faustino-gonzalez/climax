925bc681b970461298340a38c699591dfa631416
faustino-gonzalez <abcdfaustino@gmail.com>
2020-03-23 14:55:59 -0600

    Updates

---
+++ precache-manifest.9d176a3b1587d0b31d6b58a9fc826490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5576ec87716b401868b2413756373d27",
+    "url": "/clima/index.html"
+  },
+  {
+    "revision": "2ec1b22d6ac9b54ddab3",
+    "url": "/clima/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "75b1c33e664dcddc6fcb",
+    "url": "/clima/static/js/2.6588869b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clima/static/js/2.6588869b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec1b22d6ac9b54ddab3",
+    "url": "/clima/static/js/main.f4741980.chunk.js"
+  },
+  {
+    "revision": "2d91342553c7f2e17ace",
+    "url": "/clima/static/js/runtime-main.85ed0c53.js"
+  }
+]);
